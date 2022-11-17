d344b178de9d39e5a48d8a56068c4660ee49c50c
Nhat Bao <nhatbaoxxd@gmail.com>
2022-11-17 15:14:31 +0700

    Add axios
    - import axios
    - Create axiosClient
    - Create postApi

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+import axiosClient from './api/axiosClient'
+
 console.log('hello from main.js')
+
+async function main() {
+  const response = await axiosClient.get('/posts')
+  console.log(response)
+}
+
+main()
