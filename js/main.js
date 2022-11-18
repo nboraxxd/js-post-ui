51488100c0d5ff80cae9e943d3ddfaf1dd395adf
Nhat Bao <nhatbaoxxd@gmail.com>
2022-11-18 08:35:35 +0700

    Axios
    - Update form data
    - config header
    - Interceptors

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 import axiosClient from './api/axiosClient'
+import postApi from './api/postApi'
 
 console.log('hello from main.js')
 
 async function main() {
-  const response = await axiosClient.get('/posts')
-  console.log(response)
+  // const response = await axiosClient.get('/posts')
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    }
+    const data = await postApi.getAll(queryParams)
+    console.log(data)
+  } catch (error) {}
+
+  await postApi.updateFormData({
+    id: 'lea2aa9l7x3a5th',
+    title: 'Error amet sit 444'
+  })
 }
 
 main()
